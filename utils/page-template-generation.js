0c9480f39771576d7b4eb07a541d34252f9fa693
David Sklar <davidhsklar@gmail.com>
2022-04-26 09:02:33 -0400

    fixed ID error

--- utils/page-template-generation.js
+++ utils/page-template-generation.js
@@ -39,7 +39,6 @@ const generateTeamCard = (member) => {
       <div class="card-body bg-danger">
         <h5 class="card-title text-light">${name}</h5>
         <h6 class="card-title text-light">
-            <i class="${role.icon}"></i>
             ${role.roleType}
         </h6>
       </div>
@@ -75,7 +74,6 @@ const generateTeamPage = (team) => {
           <meta http-equiv="X-UA-Compatible" content="ie=edge">
           <title>The People You Work With or Something</title>
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
           <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
           <link rel="stylesheet" href="style.css">
         </head>
